34b2d6aea2361025e5b5ee7d5feac688df78636c
Romanojp <jp_romana@outlook.com>
2022-10-03 22:51:14 -0300

    criando a entidade oferta e mapeando muitos para um

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -14,3 +14,7 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
 INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES('Bootcamp HTML','http://www.hiperbytes.com.br/wp-content/uploads/2021/02/banner-curso-html.png', 'https://cdn-icons-png.flaticon.com/512/2000/2000887.png');
+
+
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:25Z', TIMESTAMP WITH TIME ZONE '2021-12-13T03:50:25Z', 1);
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2021-02-13T20:50:25Z', TIMESTAMP WITH TIME ZONE '2021-12-13T03:50:25Z', 1);
